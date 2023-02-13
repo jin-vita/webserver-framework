ed08a779dc05bd755b0c8f1c69c3933e81563d49
jin-vita <vita.unsnetworks@gmail.com>
2023-02-13 16:51:31 +0900

    modules

--- controllers/student-controller.js
+++ controllers/student-controller.js
@@ -4,89 +4,90 @@ const logger = require(`../util/logger`);
 
 const Database = require(`../database/database_mysql`);
 const studentSql = require(`../database/sql/student_sql`);
+
 /**
  * @Controller(path="/student")
  */
 class StudentController {
-  constructor() {
-    this.database = new Database(`database_mysql`);
-  }
-
-  /**
-   * @RequestMapping(path="/list", method="get,post")
-   */
-  async studentList(req, res) {
-    logger.debug(`studentList 요청됨.`);
-    const params = param.parse(req);
-
-    try {
-      // 레코드 개수 조회
-      let sql = param.getCountSql(studentSql.student_list, params);
-      let queryParam = {
-        sql: sql,
-      };
-      let rows = await this.database.query(queryParam);
-      const total = rows[0].total;
-      // SQL 문 실행
-      sql = param.getSql(studentSql.student_list, params);
-      queryParam = {
-        sql: sql,
-      };
-      rows = await this.database.query(queryParam);
-
-      // 응답 보내기
-      const output = {
-        header: {
-          requestCode: params.requestCode,
-          page: params.page,
-          perPage: params.perPage,
-          total: total,
-          search: params.search,
-          searchValue: params.searchValue,
-          searchJoin: params.searchJoin,
-          searchLike: params.searchLike,
-          order: params.order,
-          orderDirection: params.orderDirection,
-        },
-        body: rows,
-      };
-      util.sendRes(res, 200, `OK`, output);
-      logger.debug(`studentList -> ${JSON.stringify(output)}`);
-    } catch (err) {
-      util.sendError(res, 400, `Error : ${err}`);
-      logger.error(`Error in StudentController:studentList -> ${err}`);
+    constructor() {
+        this.database = new Database(`database_mysql`);
     }
-  }
 
-  /**
-   * @RequestMapping(path="/insert", method="get,post")
-   */
-  async studentInsert(req, res) {
-    logger.debug(`studentInsert 요청됨.`);
-    const params = param.parse(req);
+    /**
+     * @RequestMapping(path="/list", method="get,post")
+     */
+    async studentList(req, res) {
+        logger.debug(`studentList 요청됨.`);
+        const params = param.parse(req);
 
-    try {
-      const queryParam = {
-        sqlName: `student_insert`,
-        params: params,
-        paramType: {},
-      };
+        try {
+            // 레코드 개수 조회
+            let sql = param.getCountSql(studentSql.student_list, params);
+            let queryParam = {
+                sql: sql,
+            };
+            let rows = await this.database.query(queryParam);
+            const total = rows[0].total;
+            // SQL 문 실행
+            sql = param.getSql(studentSql.student_list, params);
+            queryParam = {
+                sql: sql,
+            };
+            rows = await this.database.query(queryParam);
 
-      let rows = await this.database.execute(queryParam);
-      // 응답 보내기
-      const output = {
-        header: {
-          requestCode: params.requestCode,
-        },
-        body: rows,
-      };
-      util.sendRes(res, 200, `OK`, output);
-      logger.debug(`studentInsert -> ${JSON.stringify(output)}`);
-    } catch (err) {
-      util.sendError(res, 400, `Error : ${err}`);
-      logger.error(`Error in StudentController:studentInsert -> ${err}`);
+            // 응답 보내기
+            const output = {
+                header: {
+                    requestCode: params.requestCode,
+                    page: params.page,
+                    perPage: params.perPage,
+                    total: total,
+                    search: params.search,
+                    searchValue: params.searchValue,
+                    searchJoin: params.searchJoin,
+                    searchLike: params.searchLike,
+                    order: params.order,
+                    orderDirection: params.orderDirection,
+                },
+                body: rows,
+            };
+            util.sendRes(res, 200, `OK`, output);
+            logger.debug(`studentList -> ${JSON.stringify(output)}`);
+        } catch (err) {
+            util.sendError(res, 400, `Error : ${err}`);
+            logger.error(`Error in StudentController:studentList -> ${err}`);
+        }
     }
-  }
+
+    /**
+     * @RequestMapping(path="/insert", method="get,post")
+     */
+    async studentInsert(req, res) {
+        logger.debug(`studentInsert 요청됨.`);
+        const params = param.parse(req);
+
+        try {
+            const queryParam = {
+                sqlName: `student_insert`,
+                params: params,
+                paramType: {},
+            };
+
+            let rows = await this.database.execute(queryParam);
+            // 응답 보내기
+            const output = {
+                header: {
+                    requestCode: params.requestCode,
+                },
+                body: rows,
+            };
+            util.sendRes(res, 200, `OK`, output);
+            logger.debug(`studentInsert -> ${JSON.stringify(output)}`);
+        } catch (err) {
+            util.sendError(res, 400, `Error : ${err}`);
+            logger.error(`Error in StudentController:studentInsert -> ${err}`);
+        }
+    }
 }
 
 module.exports = StudentController;
