fb4d3c1ade866f4b108835b98d69d9eebac65a94
jin-vita <vita.unsnetworks@gmail.com>
2023-02-16 19:15:10 +0900

    user

--- database/sql/user_sql.js
+++ database/sql/user_sql.js
@@ -1,17 +1,16 @@
 module.exports = {
-  user_list: {
-    sql: `select *
-          from test.user`,
-    count: `select count(*) as total
-            from test.student`,
-    where: `where #`,
-    order: `order by #`,
-    page: `limit #`,
-  },
-  user_insert: {
-    sql: `insert into test.user
-            (id, password, name, age, mobile, email, create_date, modify_date) 
-          values
-            (:id, :password, :name, :age, :mobile, :email, now(), now())`,
-  },
+    user_list: {
+        sql: `select *
+              from test.user`,
+        count: `select count(*) as total
+                from test.user`,
+        where: `where #`,
+        order: `order by #`,
+        page: `limit #`,
+    },
+    user_insert: {
+        sql: `insert into test.user
+                  (email, password, name, age, mobile, create_date, modify_date)
+              values (:email, :password, :name, :age, :mobile, now(), now())`,
+    },
 };
